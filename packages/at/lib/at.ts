2e03571c46e7418cdeb5b521b1654dfe23ece3e7
weiyanhai <weiyanhai@jd.com>
2023-11-02 19:54:58 +0800

    feat: lib

--- packages/at/lib/at.ts
+++ packages/at/lib/at.ts
@@ -1,7 +1,12 @@
+import { invariant } from 'ts-invariant'
 import { CanvasKit } from 'canvaskit-wasm'
 
-export interface At {
-  Sk: CanvasKit
+
+
+export enum AtStateKind {
+  Uninitialized = 1,
+  Initializing = 2,
+  Initialized = 4,
 }
 
 
@@ -10,6 +15,25 @@ export interface At {
 //   baseURL: '' 
 // })
 // at.start()
+export class AtInstance {
+  public _sk: CanvasKit | null = null
+  public get sk (): CanvasKit {
+    invariant(this._sk, `Not yet initialized sk.`)
+    return this._sk
+  }
+  public set (sk: CanvasKit) {
+    this._sk = sk
+  }
+  
+  public state: AtStateKind = AtStateKind.Uninitialized
+
+  ensure (): Promise<void> {
+    if (AtStateKind.Initialized) {
+      return Promise.resolve()
+    } else if ()
+  }
+}
+
 export class At {
-  init 
+  
 }
